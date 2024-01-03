79366cf176c799a857342c70dd8d3e3c5a4e72a4
paul fabre <paulfabre31@gmail.com>
2024-01-03 22:11:25 +0000

    test-intro-photo

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -1,5 +1,6 @@
 import Video from "./youtube-video";
 import type Author from "../interfaces/author";
+import CoverImage from "./cover-image";
 
 type Props = {
   title: string;
@@ -20,6 +21,18 @@ const HeroPost = ({
 }: Props) => {
   return (
     <section>
+      <div className="intro-photo">
+        <CoverImage
+          title="Omeara, Nov. 2023"
+          src="/assets/photos/omeara.jpg"
+        ></CoverImage>
+        <div className="intro-photo-info flex justify-between">
+          <div className="intro-photo-title m-3">Omeara, Nov. 2023</div>
+          <div className="intro-photo-credits m-3 text-end">
+            © Kadeen-Mae Brown
+          </div>
+        </div>
+      </div>
       <div className="mb-8 md:mb-16 video-block">
         <Video
           title={title}
