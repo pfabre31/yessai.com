e1384e014b13d9733d0bdbeb577ac42237895252
paul fabre <paulfabre31@gmail.com>
2024-06-16 22:58:28 +0100

    up2

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
-export const HOME_OG_IMAGE_URL = "/assets/meta_cover.png";
+export const HOME_OG_IMAGE_URL = "/assets/meta_cover2.png";
