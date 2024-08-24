879df805146cd0747bbd9af9a4c0cb5dbf8bf6d4
paul fabre <paulfabre31@gmail.com>
2024-08-24 17:22:45 +0100

    nex title

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,45 +8,45 @@ const Intro = ({ language }) => {
         <div className="flex main-title-word">
           {" "}
           <div
-            style={{ color: "#8515d5" /*"#670ed5"*/ }}
+            style={{ color: "#8515d500" /*"#670ed5"*/ }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             M
           </div>
           <div
-            style={{ color: "#891414" }}
+            style={{ color: "#89141400" }}
             className="flicker-fast main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             a
           </div>
           <div
             style={{
-              color: "rgb(109 30 163)" /*"#8515d5"*/,
+              color: "rgba(109,30,163,0)" /*"#8515d5"*/,
             }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             s
           </div>
           <div
-            style={{ color: "rgb(51 8 105)" /*"#8515d5" "#670ed5"*/ }}
+            style={{ color: "rgb(51,8,105,0)" /*"#8515d5" "#670ed5"*/ }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             t
           </div>
           <div
-            style={{ color: "#891414" }}
+            style={{ color: "#89141400" }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             e
           </div>
           <div
-            style={{ color: "rgb(51 8 105)" /*"#8515d5" "#670ed5"*/ }}
+            style={{ color: "rgba(51,8,105,00)" /*"#8515d5" "#670ed5"*/ }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             r
           </div>
           <div
-            style={{ color: "rgb(51 8 105)" /*"#8515d5" "#670ed5"*/ }}
+            style={{ color: "rgba(51,8,105,0)" /*"#8515d5" "#670ed5"*/ }}
             className="flicker-slow main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             s
@@ -55,13 +55,13 @@ const Intro = ({ language }) => {
         <div className="flex main-title-word">
           {" "}
           <div
-            style={{ color: "#891414" }}
+            style={{ color: "#89141400" }}
             className="main-title1 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             O
           </div>
           <div
-            style={{ color: "#891414" }}
+            style={{ color: "#89141400" }}
             className="main-title1 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             f
@@ -70,25 +70,32 @@ const Intro = ({ language }) => {
         <div className="flex main-title-word">
           {" "}
           <div
-            style={{ color: "#8515d5" /*"#670ed5"*/ }}
+            style={{ color: "#8515d500" /*"#670ed5"*/ }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             L
           </div>
           <div
-            style={{ color: "#891414" }}
-            className="flicker-fast main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
+            style={{ color: "#89141400", marginRight: "0.1rem" }}
+            className="flicker-fast main-title2 red-o text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             o
           </div>
+          {/* <div>
+            {" "}
+            <img
+              src="/assets/icons/pixel-heart.svg"
+              className="red-heart"
+            ></img>
+          </div> */}
           <div
-            style={{ color: "rgb(51 8 105)" /*"#8515d5" "#670ed5"*/ }}
+            style={{ color: "rgb(51,8,105,0)" /*"#8515d5" "#670ed5"*/ }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             v
           </div>
           <div
-            style={{ color: "#8515d5" /*"#670ed5"*/ }}
+            style={{ color: "#8515d500" /*"#670ed5"*/ }}
             className="main-title2 text-5xl md:text-8xl font-bold tracking-tighter leading-tight"
           >
             e
@@ -96,12 +103,9 @@ const Intro = ({ language }) => {
         </div>
       </div>
       <div></div>{" "}
-      <h4
-        style={{ color: "rgb(71 27 5)" }}
-        className="music-description text-center md:text-left text-lg"
-      >
+      {/* <h4 className="music-description text-center md:text-left text-lg">
         {versions.subtitle[language]}
-      </h4>
+      </h4> */}
       {/* <h5 className="london">London</h5> */}
     </section>
   );
