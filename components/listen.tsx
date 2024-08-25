12dbcecbaf3502d8e8036fa68eabf8b78eb700dd
paul fabre <paulfabre31@gmail.com>
2024-08-25 09:24:24 +0100

    lowkey release links

--- components/listen.tsx
+++ components/listen.tsx
@@ -17,35 +17,47 @@ const Listen = ({ language }: Props) => {
         {versions.listen.title[language]}
       </h2>
       <div className="platforms-logos flex gap-10 justify-center">
-        <a href="#" target="_blank">
+        <a
+          href="https://open.spotify.com/track/5epZ4ajHWzFGKlEolD3iH9?si=850e08ac04064373"
+          target="_blank"
+        >
           {" "}
           <img
             className="h-[4.5rem] w-[4.5rem]"
             src="/assets/icons/icon-spotify.svg"
           ></img>{" "}
         </a>
-        <a href="#" target="_blank">
+        <a
+          href="https://music.apple.com/be/album/low-key-single/1762878184"
+          target="_blank"
+        >
           {" "}
           <img
             className="h-[4.5rem] w-[4.5rem]"
             src="/assets/icons/icon-apple-music.svg"
           ></img>{" "}
         </a>
-        <a href="#" target="_blank">
+        <a
+          href="https://music.amazon.fr/tracks/B0DD4GV74H?marketplaceId=A13V1IB3VIYZZH&musicTerritory=FR&ref=dm_sh_5VfVxxMo3FfAkHvObrCmabjWH"
+          target="_blank"
+        >
           {" "}
           <img
             className="h-[4.5rem] w-[4.5rem]"
             src="/assets/icons/icon-amazon-music.svg"
           ></img>{" "}
         </a>
-        <a href="#" target="_blank">
+        <a
+          href="https://music.youtube.com/watch?v=3AxzBMAB5Yc&si=vvOaTdICKyljWk4P"
+          target="_blank"
+        >
           {" "}
           <img
             className="h-[4.5rem] w-[4.5rem]"
             src="/assets/icons/icon-youtube-music.svg"
           ></img>{" "}
         </a>
-        <a href="#" target="_blank">
+        <a href="https://deezer.page.link/jt7B86Mp6bAyJ9wb9" target="_blank">
           {" "}
           <img
             className="h-[4.5rem] w-[4.5rem]"
