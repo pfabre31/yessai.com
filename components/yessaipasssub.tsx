1f7f2e5913447c6d049c265541bb3db3aca16a90
paul fabre <paulfabre31@gmail.com>
2024-10-17 22:49:39 +0100

    imp yessaipass

--- components/yessaipasssub.tsx
+++ components/yessaipasssub.tsx
@@ -10,10 +10,7 @@ type Props = {
 const YessaiPassSub = ({ language }: Props) => {
   return (
     <section className="mb-10 mt-2">
-      <a
-        href="https://events.mastersoflovemusic.com/event/stmolml"
-        target="_blank"
-      >
+      <a href="https://buy.stripe.com/5kA7treBp44V8ZGdQS" target="_blank">
         <button className="ticket-button bg-transparent text-white font-bold py-2 px-4 rounded section-title">
           {versions.yessaipass.buttonLabel[language]}
         </button>
